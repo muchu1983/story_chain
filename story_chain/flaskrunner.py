8b9f770c4a0f6071c0a27fc76c8ea464f6fc69e0
muchu hsu <muchu1983@gmail.com>
2016-04-12 01:11:55 +0800

    add templates/

--- story_chain/flaskrunner.py
+++ story_chain/flaskrunner.py
@@ -8,11 +8,17 @@ This file is part of BSD license
 """
 from flask import Flask
 from flask import request
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/hello/<username>/<int:num>", methods=["GET", "POST"])
 def hello(username, num):
     return "Hello World! %s %d method: %s"%(username, num, request.method)
+    
+@app.route("/temp/")
+@app.route("/temp/<name>")
+def temp(name=None):
+    return render_template("temp.html", name=name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
