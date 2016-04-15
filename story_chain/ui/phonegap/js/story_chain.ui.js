03f2f1a9d841a02c80a9646d0a75d5aa254599ab
muchu hsu <muchu1983@gmail.com>
2016-04-15 21:42:50 +0800

    add index.css and scale ui-content height to 100% screen height

--- story_chain/ui/phonegap/js/story_chain.ui.js
+++ story_chain/ui/phonegap/js/story_chain.ui.js
@@ -6,6 +6,22 @@ This file is part of BSD license
 <https://opensource.org/licenses/BSD-3-Clause>
 */
 (function($) {
+    /* 內容畫面高度設定 */
+    function ScaleContentToDevice(){
+        scroll(0, 0);
+        var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
+        $(".ui-content").height(content);
+    }
+    /* 調整內容畫面高度 - pagecontainershow */
+    $(document).on("pagecontainershow", function(){
+        ScaleContentToDevice();        
+    });
+    /* 調整內容畫面高度 - resize orientationchange */
+    $(window).on("resize orientationchange", function(){
+        ScaleContentToDevice();
+    });
+
+    
     $(document).ready(function(){
         
         
